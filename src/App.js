b4d3dee14c658949832c6d17144f9eeef97c7be0
Grover Richardson <groverrichardson1987@gmail.com>
2020-05-17 09:44:53 -0700

    Completed all functions and basic functionality

--- src/App.js
+++ src/App.js
@@ -5,46 +5,123 @@ import "normalize.css";
 import "./App.css";
 
 class App extends React.Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
             tasks: Tasks.tasks,
+            numberOfTasks: Tasks.tasks.length,
+            inputValue: "",
         };
     }
 
-    addNote = () => {
-        // Add blank note field with text input
-        console.log("Note added!");
+    addTask = () => {
+        const taskNumber = this.state.numberOfTasks + 1;
+        const newTask = {
+            id: taskNumber,
+            description: `Task #${taskNumber}`,
+            editModeOn: true,
+        };
+        this.turnOffEditMode(newTask);
+        this.updateInputValue(`Task #${taskNumber}`);
+        const currentArray = this.state.tasks;
+        this.setState({
+            tasks: currentArray.concat(newTask),
+            numberOfTasks: taskNumber,
+        });
     };
 
-    deleteNote = () => {
-        // Delete note in tasks array in state
+    deleteTask = (task) => {
+        const taskNumber = this.state.numberOfTasks - 1;
+        this.setState({
+            tasks: this.state.tasks.filter((item) => item.id !== task.id),
+            numberOfTasks: taskNumber,
+        });
     };
 
-    editNote = () => {
-        /* Replace note description with text box prefilled with current note description. When you press enter, note will update with input.  */
+    updateInputValue = (value) => {
+        this.setState({
+            inputValue: value,
+        });
+        console.log("Value updated");
     };
 
-    updateEditMode = (task) => {
-        // on button click update state edit mode to on
-        console.log("Now in Edit Mode...");
+    turnOffEditMode = (task) => {
+        this.state.tasks.map((item) => {
+            if (item.id !== task.id) {
+                item.editModeOn = false;
+            }
+        });
+    };
+
+    editTask = (task) => {
+        //Take the current description and update inputValue in state and have the text of input field prefilled with input value.
+        this.turnOffEditMode(task);
+        const currentValue = task.description;
+
+        this.updateInputValue(currentValue);
+
+        const updatedArray = this.state.tasks.map((item) => {
+            if (item.id === task.id) {
+                item.editModeOn = true;
+                item.description = currentValue;
+            }
+            return item;
+        });
+        this.setState({
+            tasks: updatedArray,
+        });
+        console.log("State Set");
+    };
+
+    saveTask = (task) => {
+        this.setState({
+            tasks: this.state.tasks.map((item) => {
+                if (item.id === task.id) {
+                    item.description = this.state.inputValue;
+                    item.editModeOn = false;
+                }
+                return item;
+            }),
+        });
+        console.log(this.state.tasks);
+    };
+
+    updateForm = (e) => {
+        this.setState({
+            inputValue: e.target.value,
+        });
+        console.log(e.target.value);
     };
 
     taskStateHandler = (task) => {
         if (task.editModeOn === true) {
             return (
                 <div className="task">
-                    <form>
-                        <input type="text" className="task-description" />
-                    </form>
+                    <input
+                        type="text"
+                        className="task-description"
+                        value={this.state.inputValue}
+                        onChange={(e) => {
+                            this.updateForm(e);
+                        }}
+                    />
+                    <button
+                        className="save button"
+                        onClick={() => {
+                            this.saveTask(task);
+                        }}
+                    >
+                        Save
+                    </button>
                     <div className="buttons">
                         <button
-                            className="button edit"
-                            onClick={this.updateEditMode(task)}
+                            className="button delete"
+                            onClick={() => {
+                                this.deleteTask(task);
+                            }}
                         >
-                            Edit
+                            Delete
                         </button>
-                        <button className="button delete">Delete</button>
                     </div>
                 </div>
             );
@@ -55,16 +132,24 @@ class App extends React.Component {
                     <div className="buttons">
                         <button
                             className="button edit"
-                            onClick={this.updateEditMode(task)}
+                            onClick={() => {
+                                this.editTask(task);
+                            }}
                         >
                             Edit
                         </button>
-                        <button className="button delete">Delete</button>
+                        <button
+                            className="button delete"
+                            onClick={() => {
+                                this.deleteTask(task);
+                            }}
+                        >
+                            Delete
+                        </button>
                     </div>
                 </div>
             );
         }
-        // If note is being edited show edited version, if not show actual note.
     };
 
     render() {
@@ -73,7 +158,7 @@ class App extends React.Component {
                 <h1 className="title">Tapos</h1>
                 <div className="headers">
                     <h2 className="task-header">Tasks</h2>
-                    <button className="add-task button" onClick={this.addNote}>
+                    <button className="add-task button" onClick={this.addTask}>
                         Add Task
                     </button>
                 </div>
@@ -81,6 +166,7 @@ class App extends React.Component {
                     <TaskList
                         tasks={this.state.tasks}
                         taskStateHandler={this.taskStateHandler}
+                        numberOfTasks={this.state.numberOfTasks}
                     />
                 </div>
             </main>
--- src/Task/Task.js
+++ src/Task/Task.js
@@ -1,18 +1,12 @@
 import React from "react";
 import "../../src/App.css";
 
-class Task extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        console.log(this.props.taskStateHandler);
-        return (
-            <div className="task">
-                {this.props.taskStateHandler(this.props.task)}
-            </div>
-        );
-    }
+function Task(props) {
+    return (
+        <div className="task-container">
+            {props.taskStateHandler(props.task)}
+        </div>
+    );
 }
 
 export default Task;
